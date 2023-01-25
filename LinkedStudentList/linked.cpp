98fe20114c8f2d2007abbfa6c0348c3caa0c3956
U-BSD\472202 <472202@SHS-H04CND2.beaverton.k12.or.us>
2023-01-25 14:28:16 -0800

    Linked Student List - Finished delete function

--- LinkedStudentList/linked.cpp
+++ LinkedStudentList/linked.cpp
@@ -11,16 +11,22 @@ Node * head = NULL;
 Student* makeStudent();
 void add(Node * c, Node * p, Node * &head, Student * student);
 void print(Node* next);
+void del(Node * &head, int idToDelete);
+int getCount(Node * head);
+void average(Node * next, int number, double avg);
 
 int main() {
 
   char input[10];
-
+  int idToDelete = 0;
+  int num = 0;
+  
   cout << "Welcome to Student Linked List! Here are the commands you can use:" << endl;
   cout << endl;
   cout << "Type \"ADD\" to add a student." << endl;
   cout << "Type \"DELETE\" to delete a student." << endl;
   cout << "Type \"PRINT\" to print out all the students." << endl;
+  cout << "Type \"AVERAGE\" to average out all GPA's of the students' GPAs." << endl;
   cout << "Type \"QUIT\" to quit." << endl;
 
   do {
@@ -37,17 +43,40 @@ int main() {
       cout << "Student has been added!" << endl;
     }
     else if (strcmp(input, "DELETE") == 0) {
-      cout << "Enter the ID of the student you want to remove: ";
-      //cin >> idToDelete;
-      cin.get();
-      //del(students, idToDelete);
+      if (head == NULL) {
+	cout << "nothing to delete" << endl;
+      }
+      else {
+	cout << "Enter the ID of the student you want to remove: ";
+        cin >> idToDelete;
+        cin.get();
+        del(head, idToDelete);
+      }
     }
     else if (strcmp(input, "PRINT") == 0) {
-      print(head);
+      if (head == NULL) {
+	cout << endl;
+	cout << "There are no students currently in the list." << endl;
+      }
+      else {
+	print(head);
+      }
     }
     else if (strcmp(input, "QUIT") == 0) {
       cout << endl;
       cout << "This program has ended." << endl;
+      delete head;
+    }
+    else if (strcmp(input, "AVERAGE") == 0) {
+      if (head == NULL) {
+	cout << endl;
+	cout << "There are no students currently in the list." << endl;
+      }
+      else {
+	num = getCount(head);
+	cout << "Number of students in the list: " << num << endl;
+	average(head, num, 0);
+      }
     }
     //if the user didn't enter a valid command
     else {
@@ -117,6 +146,70 @@ void print(Node* next) {
     cout << endl;
     next->getStudent()->getDescription();
     print(next->getNext());
+  }
+  
+}
+
+void del(Node * &head, int idToDelete) {
+
+  Node * current = head;
+  Node * previous = current;
+  int counter = 0;
+  
+  while (current != NULL) {
+    if (current->student->id == idToDelete && counter == 0) {
+      Node * headRemover = current;
+      previous = current->next;
+      current = current->next;
+      head = current;
+      delete headRemover;
+    }
+    else if (current->student->id == idToDelete) {
+      Node * temp = current;
+      previous->next = current->next;
+      current = current->next;
+      delete temp;
+    }
+    else {
+      previous = current;
+      current = current->next;
+    }
+    counter++;
+  }
+
+  cout << endl;
+  cout << "Student successfully deleted!" << endl;
+  
+}
+
+int getCount(Node * head) {
+
+  int counter = 0;
+  Node * current = head;
+  
+  while (current != NULL) {
+    counter++;
+    if (current->next == NULL) {
+      return counter;
+    }
+  }
+
+  return 0;
+  
+}
+
+void average(Node * next, int number, double avg) {
+  
+  if (next == head) {
+    //do nothing
+  }
+  if (next != NULL) {
+    avg += next->student->gpa;
+    average(next->getNext(), number, avg);
+  }
+  else {
+    avg = avg / number;
+    cout << "Average GPA of students: " << avg << endl;
   }
   
 }
--- LinkedStudentList/node.h
+++ LinkedStudentList/node.h
@@ -5,6 +5,8 @@ using namespace std;
 
 class Node{
  public:
+  Student * student;
+  Node * next;
   Node(Student* student);
   Node();
   ~Node();
@@ -12,10 +14,5 @@ class Node{
   Student* getStudent(); //Returns the value of a student found in a node
   void setNext(Node* next); //Sets the next node
   Node* getNext(); //Returns the next node(end of list if NULL)
-
- private:
-  //Node values:
-  Student* student;
-  Node* next;
 };
 
