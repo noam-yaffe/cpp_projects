586e7ab874730ee065f69da8a0a31a3c68c73f60
Noam Yaffe <noamyaffe19@gmail.com>
2023-05-01 02:41:50 +0100

    Red Black Tree - All cases finally working, file input not working

--- RBTree/main.cpp
+++ RBTree/main.cpp
@@ -8,345 +8,351 @@ using namespace std;
 
 void fileGenerator();
 void manualGenerator();
-void insert(Node * &current, int num);
-void print(Node * current, int layer);
-void recolor(Node * &current);
-void checkForCases(Node * &current);
-void redUncleRightParent(Node * &current);
-void redUncleLeftParent(Node * &current);
-void rotateRight(Node * &x);
-void rotateLeft(Node * &x);
+void insert(Node*& current, int num);
+void print(Node* current, int layer);
+void recolor(Node*& current);
+void checkForCases(Node*& current);
+void redUncleRightParent(Node*& current);
+void redUncleLeftParent(Node*& current);
+Node * rotateRight(Node* x);
+Node * rotateLeft(Node* x);
 
-Node * root = NULL;
+Node* root = NULL;
 
 int main() {
 
-  char input[20];
-  int answer = 0;
-  bool cont = false;
+    char input[20];
+    int answer = 0;
+    bool cont = false;
 
-  cout << endl;
-  cout << "Welcome to Binary Search Tree!" << endl;
-
-  do {
     cout << endl;
-    cout << "Type 1 to generate the numbers with a file (automatically generates 10 random numbers), type 2 to generate them manually\
+    cout << "Welcome to Binary Search Tree!" << endl;
+
+    do {
+        cout << endl;
+        cout << "Type 1 to generate the numbers with a file (automatically generates 10 random numbers), type 2 to generate them manually\
 : ";
-    cin >> answer;
-    cin.get();
-    //generate through file input
-    if (answer == 1) {
-      fileGenerator();
-      cont = true;
-    }
-    //generate through user input
-    else if (answer == 2) {
-      manualGenerator();
-      cont = true;
-    }
-    //invalid input
-    else {
-      cout << endl;
-      cout << "You didn't enter a valid choice! Try again." << endl;
-    }
-  } while(!cont);
+        cin >> answer;
+        cin.get();
+        //generate through file input
+        if (answer == 1) {
+            fileGenerator();
+            cont = true;
+        }
+        //generate through user input
+        else if (answer == 2) {
+            manualGenerator();
+            cont = true;
+        }
+        //invalid input
+        else {
+            cout << endl;
+            cout << "You didn't enter a valid choice! Try again." << endl;
+        }
+    } while (!cont);
 
-  cout << endl;
-  print(root, 0);
-
-  cout << endl;
-  cout << "Here are the commands you can use:" << endl;
-  cout << "Type \"ADD\" to add a number to the tree;" << endl;
-  cout << "Type \"PRINT\" to print out the tree." << endl;
-  cout << "Type \"QUIT\" to end the program." << endl;
-
-  int actionNumber = 0;
-
-  //commands
-  do {
     cout << endl;
-    cout << "Enter a command: ";
-    cin.get(input, 20);
-    cin.get();
-    //add a number
-    if (strcmp(input, "ADD") == 0) {
-      cout << endl;
-      cout << "Enter the number you want to add: ";
-      cin >> actionNumber;
-      cin.get();
-      insert(root, actionNumber);
-      cout << "Number has been added!" << endl;
-    }
-    
-    //print out the tree
-    else if (strcmp(input, "PRINT") == 0) {
-      cout << endl;
-      print(root, 0);
-    }
-    //end the program
-    else if (strcmp(input, "QUIT") == 0) {
-      cout << endl;
-      cout << "The program has ended." << endl;
-    }
-    else {	
-      cout << "You didn't enter a valid command. Try again." << endl;
-    }
-  } while (strcmp(input, "QUIT") != 0);
-  
+    print(root, 0);
+
+    cout << endl;
+    cout << "Here are the commands you can use:" << endl;
+    cout << "Type \"ADD\" to add a number to the tree;" << endl;
+    cout << "Type \"PRINT\" to print out the tree." << endl;
+    cout << "Type \"QUIT\" to end the program." << endl;
+
+    int actionNumber = 0;
+
+    //commands
+    do {
+        cout << endl;
+        cout << "Enter a command: ";
+        cin.get(input, 20);
+        cin.get();
+        //add a number
+        if (strcmp(input, "ADD") == 0) {
+            cout << endl;
+            cout << "Enter the number you want to add: ";
+            cin >> actionNumber;
+            cin.get();
+            insert(root, actionNumber);
+            cout << "Number has been added!" << endl;
+        }
+
+        //print out the tree
+        else if (strcmp(input, "PRINT") == 0) {
+            cout << endl;
+            print(root, 0);
+        }
+        //end the program
+        else if (strcmp(input, "QUIT") == 0) {
+            cout << endl;
+            cout << "The program has ended." << endl;
+        }
+        else {
+            cout << "You didn't enter a valid command. Try again." << endl;
+        }
+    } while (strcmp(input, "QUIT") != 0);
+
 }
 
 //generate initial binary tree via random file input
 void fileGenerator() {
 
-  cout << "file generation..." << endl;
-  
-  ifstream numbers("numbers.txt");
-  srand(time(NULL));
-  int num = 0;
-  vector<int> nums;
+    cout << "file generation..." << endl;
 
-  while (!numbers.eof()) {
-    numbers >> num;
-    nums.push_back(num);
-  }
+    ifstream numbers("numbers.txt");
+    srand(time(NULL));
+    int num = 0;
+    vector<int> nums;
 
-  int randomIndex = 0;
-  int counter = 0;
-  
-  while (counter != 10) {
-    randomIndex = (rand() % 999) + 1;
-    num = nums.at(randomIndex);
-    cout << num << " ";
-    insert(root, num);
-    counter++;
-  }
-  cout << endl;
-  
+    cout << "test" << endl;
+
+    while (!numbers.eof()) {
+        numbers >> num;
+        nums.push_back(num);
+    }
+
+    cout << "test" << endl;
+
+    int randomIndex = 0;
+    int counter = 0;
+
+    while (counter != 10) {
+        randomIndex = (rand() % 999) + 1;
+        num = nums.at(randomIndex);
+        cout << num << " ";
+        insert(root, num);
+        counter++;
+    }
+    cout << endl;
+
 }
 
 //generate initial binary tree via by user input
 void manualGenerator() {
 
-  int input = 0;
-  int counter = 0;
+    int input = 0;
+    int counter = 0;
 
-  cout << endl;
-  cout << "You will be prompted to manually enter numbers one-by-one." << endl;
-  cout << "You can terminate the loop by entering the number -1, but it automatically ends after you've entered a total of 50 numbers\
+    cout << endl;
+    cout << "You will be prompted to manually enter numbers one-by-one." << endl;
+    cout << "You can terminate the loop by entering the number -1, but it automatically ends after you've entered a total of 50 numbers\
 ." << endl;
 
-  while (input != 0 || counter < 50) {
-    cout << endl;
-    cout << "Enter a number: ";
-    cin >> input;
-    if (input == -1) {
-      break;
+    while (input != 0 || counter < 50) {
+        cout << endl;
+        cout << "Enter a number: ";
+        cin >> input;
+        if (input == -1) {
+            break;
+        }
+        else {
+            insert(root, input);
+            cout << endl;
+            cout << "Number has been entered into the tree!" << endl;
+            counter++;
+        }
     }
-    else {
-      insert(root, input);
-      cout << endl;
-      cout << "Number has been entered into the tree!" << endl;
-      counter++;
-    }
-  }
 
-  cin.get();
-  
+    cin.get();
+
 }
 
 //add data into the tree
-void insert(Node * &current, int num) {
-  
-  if (current == NULL) {//root is NULL
-    current = new Node(num, NULL, NULL, NULL, 'B');
-    root = current;
-    return;
-  }
-  //greater than or equal to the current node
-  if (num >= current->data) {
-    if (current->right == NULL) {
-      current->right = new Node(num, NULL, NULL, current, 'R');
-      checkForCases(current->right);
-      return;
+void insert(Node*& current, int num) {
+
+    if (current == NULL) {//root is NULL
+        current = new Node(num, NULL, NULL, NULL, 'B');
+        root = current;
+        return;
     }
+    //greater than or equal to the current node
+    if (num >= current->data) {
+        if (current->right == NULL) {
+            current->right = new Node(num, NULL, NULL, current, 'R');
+            checkForCases(current->right);
+            return;
+        }
+        else {
+            insert(current->right, num);
+        }
+    }
+    //less than the current node
     else {
-      insert(current->right, num);
+        if (current->left == NULL) {
+            current->left = new Node(num, NULL, NULL, current, 'R');
+            checkForCases(current->left);
+            return;
+        }
+        else {
+            insert(current->left, num);
+        }
     }
-  }
-  //less than the current node
-  else {
-    if (current->left == NULL) {
-      current->left = new Node(num, NULL, NULL, current, 'R');
-      checkForCases(current->left);
-      return;
-    }
-    else {
-      insert(current->left, num);
-    }
-  }
 
 }
 
 //print out the tree (using the inOrder traversal algorithm)
-void print(Node * current, int layer) {
+void print(Node* current, int layer) {
 
-  if (current == NULL) {
-    return;
-  }
+    if (current == NULL) {
+        return;
+    }
 
-  print(current->right, layer + 1);
+    print(current->right, layer + 1);
 
-  for (int i = 0; i < layer; i++) {
-    cout << "    ";
-  }
-  
-  cout << current->data << current->color << endl;
+    for (int i = 0; i < layer; i++) {
+        cout << "    ";
+    }
 
-  print(current->left, layer + 1);
-  
+    cout << current->data << current->color << endl;
+
+    print(current->left, layer + 1);
+
 }
 
-void recolor(Node * &current) {
+void recolor(Node*& current) {
 
-  if (current->color == 'R') {//change from red to black
-    current->color = 'B';
-  }
-  else {//change from black to red
-    current->color = 'R';
-  }
-  
+    if (current->color == 'R') {//change from red to black
+        current->color = 'B';
+    }
+    else {//change from black to red
+        current->color = 'R';
+    }
+
 }
 
-void checkForCases(Node * &current) {
+void checkForCases(Node*& current) {
 
-  if (current->parent == NULL || current->parent->parent == NULL) {//base case, no grandparent-->no uncle-->no cases to check for
-    return;
-  }
-  
-  Node * grandparent = current->parent->parent;
-  
-  if (grandparent->right == current->parent) {//right parent
-    Node * uncle = grandparent->left;
-    if ((uncle != NULL) && (uncle->color == 'R')) {//uncle is red
-      cout << "uncle is red" << endl;
-      redUncleRightParent(current);
-      checkForCases(grandparent);//recursive call on grandparent
+    if (current == NULL || current->parent == NULL || current->parent->parent == NULL) {//base case, no grandparent-->no uncle-->no cases to check for
+        return;
     }
-    else if ((uncle == NULL) || (uncle->color == 'B') && current->parent->color == 'R') {//black uncle cases
-      cout << "uncle is black" << endl;
-      if (current->parent->left == current) {//current is a left child, parent is a right child (triangle case)
-	cout << "current is left, parent is right (triangle)" << endl;
-	Node * originalParent = current->parent;
-	rotateRight(current->parent);
-	checkForCases(originalParent);
-      }
-      else {//current is a right child, parent is a right child (line case)
-	cout << "current is right, parent is right (line)" << endl;
-	recolor(current->parent);
-	recolor(grandparent);
-	rotateLeft(grandparent);
-	checkForCases(grandparent);
-      }
+
+    Node* grandparent = current->parent->parent;
+
+    if (grandparent->right == current->parent) {//right parent
+        Node* uncle = grandparent->left;
+        if ((uncle != NULL) && (uncle->color == 'R')) {//uncle is red
+            //cout << "uncle is red" << endl;
+            redUncleRightParent(current);
+            checkForCases(grandparent);//recursive call on grandparent
+        }
+        else if (((uncle == NULL) || (uncle->color == 'B')) && current->parent->color == 'R') {//black uncle cases
+            //cout << "uncle is black" << endl;
+            if (current->parent->left == current) {//current is a left child, parent is a right child (triangle case)
+                //cout << "current is left, parent is right (triangle)" << endl;
+                Node * temp = rotateRight(current->parent);
+                checkForCases(temp->right);
+            }
+            else {//current is a right child, parent is a right child (line case)
+                //cout << "current is right, parent is right (line)" << endl;
+                recolor(current->parent);
+                recolor(grandparent);
+                Node * temp = rotateLeft(current->parent->parent);
+                checkForCases(grandparent);
+            }
+        }
+        if (root->color == 'R') {
+            recolor(root);
+        }
+        return;
     }
-    if (root->color == 'R') {
-      recolor(root);
+    else {//left parent
+        Node* uncle = grandparent->right;
+        if ((uncle != NULL) && (uncle->color == 'R')) {//uncle is red
+            //cout << "uncle is red!" << endl;
+            redUncleLeftParent(current);
+            checkForCases(grandparent);//recursive call on grandparent
+        }
+        else if (((uncle == NULL) || (uncle->color == 'B')) && current->parent->color == 'R') {//black uncle cases
+            //cout << "uncle is black" << endl;
+            if (current->parent->right == current) {//current is a right child, parent is a left child (triangle case)
+                //cout << "current is right, parent is left (triangle)" << endl;
+                Node * temp = rotateLeft(current->parent);
+                checkForCases(temp->left);
+            }
+            else {//current is a left child, parent is a left child (line case)
+                //cout << "current is left, parent is left (line)" << endl;
+                recolor(current->parent);
+                recolor(grandparent);
+                Node * temp = rotateRight(current->parent->parent);
+                checkForCases(grandparent);
+            }
+        }
+        if (root->color == 'R') {
+            recolor(root);
+        }
+        return;
     }
-    return;
-  }
-  else {//left parent
-    Node * uncle = grandparent->right;
-    if ((uncle != NULL) && (uncle->color == 'R')) {//uncle is red
-      cout << "uncle is red!" << endl;
-      redUncleLeftParent(current);
-      checkForCases(grandparent);//recursive call on grandparent
-    }
-    else if ((uncle == NULL) || (uncle->color == 'B') && current->parent->color == 'R') {//black uncle cases
-      cout << "uncle is black" << endl;
-      if (current->parent->right == current) {//current is a right child, parent is a left child (triangle case)
-	cout << "current is right, parent is left (triangle)" << endl;
-	Node * originalParent = current->parent;
-	rotateLeft(current->parent);
-	checkForCases(originalParent);
-      }
-      else {//current is a left child, parent is a left child (line case)
-	cout << "current is left, parent is left (line)" << endl;
-	recolor(current->parent);
-	recolor(grandparent);
-	rotateRight(grandparent);
-	checkForCases(grandparent);
-      }
-    }
-    if (root->color == 'R') {
-      recolor(root);
-    }
-    return;
-  }
 
 }
 
 //recolors current's parent, grandparent, and uncle (specific to right parent)
-void redUncleRightParent(Node * &current) {
+void redUncleRightParent(Node*& current) {
 
-  recolor(current->parent);
-  recolor(current->parent->parent);
-  recolor(current->parent->parent->left);
-  
+    recolor(current->parent);
+    recolor(current->parent->parent);
+    recolor(current->parent->parent->left);
+
 }
 
 //recolors current's parent, grandparent, and uncle (specific to left parent)
-void redUncleLeftParent(Node * &current) {
+void redUncleLeftParent(Node*& current) {
 
-  recolor(current->parent);
-  recolor(current->parent->parent);
-  recolor(current->parent->parent->right);
-  
+    recolor(current->parent);
+    recolor(current->parent->parent);
+    recolor(current->parent->parent->right);
+
 }
 
 //NOTE: both my "rotateRight" and "rotateLeft" algorithms were referenced from the "CodesDope" website
 //https://www.codesdope.com/course/data-structures-red-black-trees-insertion/
 
 //right rotation
-void rotateRight(Node * &x) {
+Node * rotateRight(Node* current) {
 
-  Node * y = x->left;
-  x->left = y->right;
-  if (y->right != NULL) {
-    y->right->parent = x;
-  }
-  y->parent = x->parent;
-  if (x->parent == NULL) {//x is the new root
-    root = y;
-  }
-  else if (x == x->parent->right) {//x is now a right child
-    x->parent->right = y;
-  }
-  else {//x is now a left child
-    x->parent->left = y;
-  }
-  y->right = x;
-  x->parent = y;
-  
+    Node* leftNode = current->left;
+    current->left = leftNode->right;
+    if (leftNode->right != NULL) {//child's right isn't null --> parent needs to be redirected
+        leftNode->right->parent = current;
+    }
+    leftNode->right = current;
+    leftNode->parent = current->parent;
+    if (current->parent == NULL) {//node will be root after rotation
+        root = leftNode;
+    }
+    else if (current == current->parent->left) {//current is a left child
+        current->parent->left = leftNode;
+    }
+    else {//current is a right child
+        current->parent->right = leftNode;
+    }
+    current->parent = leftNode;
+
+    return leftNode;
+
 }
 
 //left rotation
-void rotateLeft(Node * &x) {
+Node * rotateLeft(Node* current) {
 
-  Node * y = x->right;
-  x->right = y->left;
-  if (y->left != NULL) {
-    y->left->parent = x;
-  }
-  y->parent = x->parent;
-  if (x->parent == NULL) {//x is the new root
-    root = y;
-  }
-  else if (x == x->parent->left) {//x is now a left child
-    x->parent->left = y;
-  }
-  else {//x is now a right child
-    x->parent->right = y;
-  }
-  y->left = x;
-  x->parent = y;
-  
+    Node* rightNode = current->right;
+    current->right = rightNode->left;
+    if (rightNode->left != NULL) {//child's left isn't null --> parent needs to be redirected
+        rightNode->left->parent = current;
+    }
+    rightNode->left = current;
+    rightNode->parent = current->parent;
+    if (current->parent == NULL) { //node will be root after rotation
+        root = rightNode;
+    }
+    else if (current == current->parent->left) { //current is a left child
+        current->parent->left = rightNode;
+    }
+    else { //current is a right child
+        current->parent->right = rightNode;
+    }
+    current->parent = rightNode;
+
+    return rightNode;
+
 }
